93e9926fce276b54d19392ef14fbd008edab9277
Yuka-Makino <ec2-user@ip-172-31-28-11.ap-northeast-1.compute.internal>
2023-06-18 05:19:50 +0000

    Form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "28e8694707fa4746bc2ff8aa93dac055.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
